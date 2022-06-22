fca90e12bba5a0c1670283159ffc55e4e36f449b
dhopz <david_houpapa@hotmail.com>
2022-06-22 15:44:06 +0100

    researching styles

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import PageLinks from '../Components/pageMenu';
-import Box from '@mui/material/Box';
+import Navigator from '../Components/Navigator';
+import Header from '../Components/Header';
+import Content from '../Components/Content';
+import Paperbase from '../Components/Paperbase';
 
 const App = () => {
     return (
-        <Box sx={{ m:4 }}>        
-            <header>
-                <h1>Windmill Inventory</h1>
-                <br/>
-                <h2>Error File Handlers</h2>
-            </header>
-            <br/>
-            <PageLinks/>      
-        </Box>
+        <Header/>,
+        <Navigator/>,
+        <Content/>,
+        <Paperbase/>        
     );
 };
 
